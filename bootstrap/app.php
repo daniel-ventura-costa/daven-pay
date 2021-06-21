e71023e4d6e1a9bd5281f5d02cee8d54a17c22e4
Danielventura90 <danielventura.90@gmail.com>
2021-06-21 12:32:27 -0300

    feat: Adicionando processamendo de fila com Redis

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -94,6 +94,7 @@ $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(App\Providers\CatchAllOptionsRequestsProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+$app->register(\Illuminate\Redis\RedisServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Carbon\Carbon;
-
 /** @var \Laravel\Lumen\Routing\Router $router */
 
 /*
